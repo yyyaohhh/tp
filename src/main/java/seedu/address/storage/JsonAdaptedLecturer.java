079feb1ffdf4b650772a400ce353bf7a8dbd974f
ji-just-ji <wongjk9999@gmail.com>
2023-10-12 09:31:54 +0800

    Update and add files to handle modules in storage
    update ModelManager javadoc
    update Module to use set instead of list
    add JsonAdaptedLecturer and JsonAdaptedModule
    update JsonSerializableAddressBook for Module

---
+++ src/main/java/seedu/address/storage/JsonAdaptedLecturer.java
@@ -0,0 +1,47 @@
+package seedu.address.storage;
+
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.module.Lecturer;
+
+/**
+ * Jackson-friendly version of {@link Lecturer}.
+ */
+public class JsonAdaptedLecturer {
+    private final String lecturerName;
+
+    /**
+     * Constructs a {@code JsonAdaptedLecturer} with the given {@code lecturerName}.
+     */
+    @JsonCreator
+    public JsonAdaptedLecturer(String lecturerName) {
+        this.lecturerName = lecturerName;
+    }
+
+    /**
+     * Converts a given {@code Lecturer} into this class for Jackson use.
+     */
+    public JsonAdaptedLecturer(Lecturer source) {
+        lecturerName = source.lecturerName;
+    }
+
+    @JsonValue
+    public String getLecturerName() {
+        return lecturerName;
+    }
+
+    /**
+     * Converts this Jackson-friendly adapted tag object into the model's {@code Tag} object.
+     *
+     * @throws IllegalValueException if there were any data constraints violated in the adapted tag.
+     */
+    public Lecturer toModelType() throws IllegalValueException {
+        if (!Lecturer.isValidLecturer(lecturerName)) {
+            throw new IllegalValueException(Lecturer.MESSAGE_CONSTRAINTS);
+        }
+        return new Lecturer(lecturerName);
+    }
+
+}
