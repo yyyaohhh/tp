9c8d40c6c7f9a96eb2ebd842a133f1d98087f8df
marquestye <marquestyes@gmail.com>
2023-10-18 18:26:27 +0800

    Fix more styling issues

--- src/main/java/seedu/address/logic/commands/InfoCommand.java
+++ src/main/java/seedu/address/logic/commands/InfoCommand.java
@@ -18,14 +18,14 @@ public class InfoCommand extends Command {
             + ": Shows information on the module identified by the module code. "
             + "Example: " + COMMAND_WORD + " " + "CS1101S ";
     public static final String MESSAGE_INFO_MODULE_SUCCESS = "%1$s: %2$s \n"
-            + "%3$s \n" 
+            + "%3$s \n"
             + "%4$s \n";
     public static final String MESSAGE_INFO_MODULE_SUCCESS_SAMPLE = "CS1101S : Programming Methodology \n"
             + "4MCs \n"
             + "This course introduces the concepts of programming and computational problem solving. "
             + "Starting from a small core of fundamental abstractions, the course introduces"
             + " programming as a method for communicating computational processes. \n";
- 
+
     public static final String MESSAGE_NOT_FOUND_MODULE = "This module is not found.";
 
     private final ModuleCode moduleCode;
