530842b26d5ac584011b03a346be9e8b298d99a6
marquestye <marquestyes@gmail.com>
2023-10-20 13:37:56 +0800

    Update DeleteCommandParser exception throwing

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -2,6 +2,9 @@ package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.module.ModuleCode;
@@ -11,20 +14,24 @@ import seedu.address.model.module.ModuleCode;
  */
 public class DeleteCommandParser implements Parser<DeleteCommand> {
 
+    // Pattern to check for single argument (no spaces)
+    private static final Pattern SINGLE_ARGUMENT_FORMAT = Pattern.compile("^[^\\s]+$");
+
     /**
      * Parses the given {@code String} of arguments in the context of the DeleteCommand
      * and returns a DeleteCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public DeleteCommand parse(String args) throws ParseException {
-        try {
-            ModuleCode code = ParserUtil.parseModuleCode(args);
-            return new DeleteCommand(code);
 
-        } catch (ParseException pe) {
+        Matcher matcher = SINGLE_ARGUMENT_FORMAT.matcher(args.trim());
+        if (!matcher.matches()) {
             throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe);
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
         }
+
+        ModuleCode moduleCode = ParserUtil.parseModuleCode(args);
+        return new DeleteCommand(moduleCode);
     }
 
 }
