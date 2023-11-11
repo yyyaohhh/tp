5108d0162df53b8fb2ea01fc677bdb1903c5fd0a
marquestye <marquestyes@gmail.com>
2023-11-11 17:40:34 +0800

    Update InfoCommandTest

--- src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
@@ -21,9 +21,7 @@ import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.module.Module;
-import seedu.address.model.module.ModuleCode;
 import seedu.address.model.moduleplan.ModulePlan;
-import seedu.address.testutil.ModuleBuilder;
 
 
 /**
