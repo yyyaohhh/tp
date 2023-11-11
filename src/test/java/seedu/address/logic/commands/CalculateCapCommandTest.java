65a298f8f45e08af1c1a9cef627b3a01a9823f83
marquestye <marquestyes@gmail.com>
2023-11-11 15:54:34 +0800

    Update CalculateCapCommandTest and CalculateMcCommandTest

--- src/test/java/seedu/address/logic/commands/CalculateCapCommandTest.java
+++ src/test/java/seedu/address/logic/commands/CalculateCapCommandTest.java
@@ -1,43 +1,52 @@
 package seedu.address.logic.commands;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalModules.IS6000;
+import static seedu.address.testutil.TypicalModules.MODULE_ZERO_MC;
 import static seedu.address.testutil.TypicalModules.getTypicalModuleData;
 import static seedu.address.testutil.TypicalModules.getTypicalModulePlan;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.moduleplan.ModulePlan;
 
 /**
- * Contains integration tests (interaction with the Model) and unit tests for CalculateCapCommand.
+ * Contains integration tests (interaction with the Model) for CalculateCapCommand.
  */
 public class CalculateCapCommandTest {
 
-    private Model model;
-    private Model expectedModel;
-    @BeforeEach
-    public void setUp() {
-        model = new ModelManager(getTypicalModulePlan(), new UserPrefs(), getTypicalModuleData());
-        expectedModel = new ModelManager(getTypicalModulePlan(), new UserPrefs(), getTypicalModuleData());
+    @Test
+    public void execute_calculateCap_success() {
+        // Both actual and expected models should be identical
+        Model actualmodel = new ModelManager(getTypicalModulePlan(), new UserPrefs(), getTypicalModuleData());
+        Model expectedModel = new ModelManager(getTypicalModulePlan(), new UserPrefs(), getTypicalModuleData());
+
+        String expectedMessage = String.format(CalculateCapCommand.MESSAGE_CALCULATION_SUCCESS, actualmodel.getCap());
+
+        assertCommandSuccess(new CalculateCapCommand(), actualmodel, expectedMessage, expectedModel);
+
+        //Test for zero MC modules as well (same expected message as above)
+        actualmodel.addModule(MODULE_ZERO_MC);
+        expectedModel.addModule(MODULE_ZERO_MC);
+        assertCommandSuccess(new CalculateCapCommand(), actualmodel, expectedMessage, expectedModel);
     }
 
     @Test
-    public void execute_calculateCap_success() {
-        CalculateCapCommand calculateCapCommand = new CalculateCapCommand();
+    public void execute_emptyModulePlan_success() {
+        // Model with empty in modulePlan
+        Model actualmodel = new ModelManager(new ModulePlan(), new UserPrefs(), getTypicalModuleData());
+        Model expectedModel = new ModelManager(new ModulePlan(), new UserPrefs(), getTypicalModuleData());
 
-        String expected = String.format(CalculateCapCommand.MESSAGE_CALCULATION_SUCCESS, model.getCap());
+        // Expected CAP should be 0.0
+        String expectedMessage = String.format(CalculateCapCommand.MESSAGE_CALCULATION_SUCCESS, 0.0);
 
-        assertCommandSuccess(calculateCapCommand, model, expected, expectedModel);
+        assertCommandSuccess(new CalculateCapCommand(), actualmodel, expectedMessage, expectedModel);
 
-        //Test for zero MC modules as well
-        model.addModule(IS6000);
-        expectedModel.addModule(IS6000);
-        assertCommandSuccess(calculateCapCommand, model, expected, expectedModel);
-
+        //Test for zero MC modules as well (same expected message as above)
+        actualmodel.addModule(MODULE_ZERO_MC);
+        expectedModel.addModule(MODULE_ZERO_MC);
+        assertCommandSuccess(new CalculateCapCommand(), actualmodel, expectedMessage, expectedModel);
     }
-
 }
--- src/test/java/seedu/address/logic/commands/CalculateMcCommandTest.java
+++ src/test/java/seedu/address/logic/commands/CalculateMcCommandTest.java
@@ -1,25 +1,53 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalModules.MODULE_ZERO_MC;
 import static seedu.address.testutil.TypicalModules.getTypicalModuleData;
 import static seedu.address.testutil.TypicalModules.getTypicalModulePlan;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.moduleplan.ModulePlan;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for CalculateMcCommand.
  */
 public class CalculateMcCommandTest {
-    private Model model;
-    private Model expectedModel;
-    @BeforeEach
-    public void setUp() {
-        model = new ModelManager(getTypicalModulePlan(), new UserPrefs(), getTypicalModuleData());
-        expectedModel = new ModelManager(getTypicalModulePlan(), new UserPrefs(), getTypicalModuleData());
+
+    @Test
+    public void execute_calculateMc_success() {
+        // Both actual and expected models should be identical
+        Model actualmodel = new ModelManager(getTypicalModulePlan(), new UserPrefs(), getTypicalModuleData());
+        Model expectedModel = new ModelManager(getTypicalModulePlan(), new UserPrefs(), getTypicalModuleData());
+
+        String expectedMessage = String.format(CalculateMcCommand.MESSAGE_CALCULATION_SUCCESS,
+                actualmodel.totalModularCredits());
+
+        assertCommandSuccess(new CalculateMcCommand(), actualmodel, expectedMessage, expectedModel);
+
+        //Test for zero MC modules as well (same expected message as above)
+        actualmodel.addModule(MODULE_ZERO_MC);
+        expectedModel.addModule(MODULE_ZERO_MC);
+        assertCommandSuccess(new CalculateMcCommand(), actualmodel, expectedMessage, expectedModel);
     }
 
+    @Test
+    public void execute_emptyModulePlan_success() {
+        // Model with empty modulePlan
+        Model actualmodel = new ModelManager(new ModulePlan(), new UserPrefs(), getTypicalModuleData());
+        Model expectedModel = new ModelManager(new ModulePlan(), new UserPrefs(), getTypicalModuleData());
 
+        // Expected MC should be 0.0
+        String expectedMessage = String.format(CalculateMcCommand.MESSAGE_CALCULATION_SUCCESS, 0.0);
+
+        assertCommandSuccess(new CalculateMcCommand(), actualmodel, expectedMessage, expectedModel);
+
+        //Test for zero MC modules as well (same expected message as above)
+        actualmodel.addModule(MODULE_ZERO_MC);
+        expectedModel.addModule(MODULE_ZERO_MC);
+        assertCommandSuccess(new CalculateMcCommand(), actualmodel, expectedMessage, expectedModel);
+    }
 }
