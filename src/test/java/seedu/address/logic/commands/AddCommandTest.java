2b0bc5c26c13d0b0837dc612e4da6a438a51425a
marquestye <marquestyes@gmail.com>
2023-11-11 15:53:16 +0800

    Fix modelStub logic in AddCommandTest

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -13,31 +13,20 @@ import static seedu.address.testutil.TypicalModules.getTypicalModuleData;
 import static seedu.address.testutil.TypicalModules.getTypicalModulePlan;
 import static seedu.address.testutil.TypicalModules.getTypicalModulePlanWithout;
 
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
-import javafx.collections.ObservableList;
-import seedu.address.commons.core.GuiSettings;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
-import seedu.address.model.ReadOnlyModuleData;
-import seedu.address.model.ReadOnlyUserPrefs;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.module.Description;
-import seedu.address.model.module.ModularCredit;
 import seedu.address.model.module.Module;
 import seedu.address.model.module.ModuleCode;
-import seedu.address.model.module.ModuleName;
 import seedu.address.model.module.exceptions.DuplicateModuleException;
 import seedu.address.model.module.exceptions.ModuleNotFoundException;
-import seedu.address.model.moduleplan.ModulePlan;
-import seedu.address.model.moduleplan.ModulePlanSemester;
-import seedu.address.model.moduleplan.ReadOnlyModulePlan;
 import seedu.address.testutil.ModelStub;
 import seedu.address.testutil.ModuleBuilder;
 
@@ -139,138 +128,14 @@ public class AddCommandTest {
     }
 
     /**
-     * A default model stub that have all of the methods failing.
+     * A Model stub that accepts multiple modules.
      */
-    private class EmptyModelStub extends ModelStub {
-        @Override
-        public void setUserPrefs(ReadOnlyUserPrefs userPrefs) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public ReadOnlyUserPrefs getUserPrefs() {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public GuiSettings getGuiSettings() {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public void setGuiSettings(GuiSettings guiSettings) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public Path getModulePlanFilePath() {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public void setModulePlanFilePath(Path addressBookFilePath) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public void setModulePlan(ReadOnlyModulePlan modulePlan) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public ReadOnlyModulePlan getModulePlan() {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public void addModule(Module module) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public boolean hasModule(Module module) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public void deleteModule(Module module) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public void setModule(Module target, Module editedModule) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public Module getModule(ModuleCode code) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public float totalModularCredits() {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public Float getCap() {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public void setModuleData(ReadOnlyModuleData moduleData) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public ReadOnlyModuleData getModuleData() {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        public ModuleName getDbModuleName(ModuleCode moduleCode) throws ModuleNotFoundException {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        public Description getDbModuleDescription(ModuleCode moduleCode) throws ModuleNotFoundException {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        public ModularCredit getDbModularCredit(ModuleCode moduleCode) throws ModuleNotFoundException {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public boolean checkDbValidModule(Module module) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public boolean checkDbValidModuleCode(ModuleCode moduleCode) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public Module getModuleFromDb(ModuleCode moduleCode) {
-            return getTypicalModuleData().getModule(moduleCode);
-        }
-
-        @Override
-        public ObservableList<ModulePlanSemester> getFilteredModuleList() {
-            throw new AssertionError("This method should not be called.");
-        }
-    }
-
-    /**
-     * A Model stub that contains a single module.
-     */
-    private class ModelStubWithMultipleModule extends EmptyModelStub {
-        private final Module module;
+    private class ModelStubWithMultipleModule extends ModelStub {
         private final ArrayList<Module> modulesAdded = new ArrayList<>();
 
 
         ModelStubWithMultipleModule(Module module) {
             requireNonNull(module);
-            this.module = module;
             modulesAdded.add(module);
         }
 
@@ -287,35 +152,10 @@ public class AddCommandTest {
             requireNonNull(module);
             return modulesAdded.stream().anyMatch(module::isSameModule);
         }
-
-
-    }
-
-    /**
-     * A Model stub that always accept the Module being added.
-     */
-    private class ModelStubAcceptingModuleAdded extends ModelStubWithMultipleModule {
-        final ArrayList<Module> modulesAdded = new ArrayList<>();
-
-        ModelStubAcceptingModuleAdded(Module module) {
-            super(module);
-        }
-
+        
         @Override
-        public boolean hasModule(Module module) {
-            requireNonNull(module);
-            return modulesAdded.stream().anyMatch(module::isSameModule);
-        }
-
-        @Override
-        public void addModule(Module module) {
-            requireNonNull(module);
-            modulesAdded.add(module);
-        }
-
-        @Override
-        public ReadOnlyModulePlan getModulePlan() {
-            return new ModulePlan();
+        public Module getModuleFromDb(ModuleCode moduleCode) {
+            return getTypicalModuleData().getModule(moduleCode);
         }
     }
 
